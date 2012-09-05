ad246e383510ffa3978ceed4e88845296d270d94
jie lian <imjeee@gmail.com>
2012-09-05 15:49:55 -0500

    cleaned up a little

--- wp-content/themes/imjeee2012/header.php
+++ wp-content/themes/imjeee2012/header.php
@@ -20,8 +20,7 @@
 	} ?></title>
 	<meta name="description" content="<?php wp_title(''); echo ' | '; bloginfo( 'description' ); ?>" />
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	<meta name="viewport" content="width=device-width; initial-scale=1"/><?php /* Add "maximum-scale=1" to fix the Mobile Safari auto-zoom bug on orientation changes, but keep in mind that it will disable user-zooming completely. Bad for accessibility. */ ?>
+
 	<link rel="icon" href="<?php bloginfo('template_url'); ?>/whiteboard_favicon.ico" type="image/x-icon" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
@@ -30,105 +29,15 @@
 	<!-- Javascript -->
 	<script src="<?php bloginfo( 'template_url' ); ?>/js/jquery-1.8.0.min.js"></script>
 	<script src="<?php bloginfo( 'template_url' ); ?>/js/jquery-ui-1.8.23.custom.min.js"></script>
-	<script src="<?php bloginfo( 'template_url' ); ?>/js/nav.js"></script>
-
-	<!-- for grid a licious -->
-	<script type="text/javascript">
-
-	var t, settings = {gap:10, width: 180, easing: 'easeInOutQuint', duration: 300, speed: 30 };
-	var nums = [ 300,  600,  250,  100 ];
-	var prevh, prevw, h, thumb;
-
-	</script>
-	<script src="<?php bloginfo( 'template_url' ); ?>/js/jquery.preload.js"></script>
-	<script src="<?php bloginfo( 'template_url' ); ?>/js/grid-a-licious.js"></script>
-	<script src="<?php bloginfo( 'template_url' ); ?>/js/jquery.easing.js"></script>
+	<script src="<?php bloginfo( 'template_url' ); ?>/js/functions.js"></script>
 
 	<script type="text/javascript">
-
-	$(document).ready(function() {	
-
-  $.preload('.post img', {
-  	placeholder:'http://suprb.com/lib/img/grey.gif',
-		threshold: 1,
-		onComplete:complete,
-		onFinish:finish
-	});
-
-	function complete(data){
-		var img = new Image();
-		img.src = data.image;
-		post = $("img[src='"+data.image+"']").parent();
-		$(post).css('height',img.height);
-	};
-
-	function finish(data){
-		$('#box1').grid(settings);
-	};
-
-  // on window resize, call again
-    var resizeTimer = null;
-    $(window).bind('resize', function() {
-      if (resizeTimer) clearTimeout(resizeTimer);
-      resizeTimer = setTimeout(function(){ 
-      		$("#box1").grid(settings, null, false);
-      }, 100 );		
-    });
-    
-
-	$(".post").live('click', function() {
-		$(t).empty();
-		$(t).animate({height:prevh, width: prevw}, 200, function(){
-				$(this).append('<img src="'+thumb+'" />');
+		$(document).ready(function(){
+		  ajaxLoadPageInit();
+		  contactFormInit();
 		});
-		
-		$(t).delay(100).removeClass('open postwide');
-		
-		t = this;
-		
-		$(this).fadeOut('fast', function() {
-		
-				$(t).append('<div class="loader"><img src="http://suprb.com/lib/img/loading.gif"></div>');
-		
-					prevh = $(t).height();
-					prevw = $(t).width();
-					thumb = $(t).find('img').attr('src');
-		
-					$(t).addClass('open postwide');
-					num = nums[Math.floor(Math.random()*nums.length)];
-					$(t).empty();
-					
-					$(t).append('<img class="pme" src="'+$(t).attr('ref')+'" />');
-					
-				  $.preload('.pme', {
-				  	placeholder:'http://suprb.com/lib/img/grey.gif',
-						threshold: 1,
-						onComplete:complete,
-						onFinish:finishh
-					});
-					function finishh($t){
-							$("#box1").grid(settings, $(t), true, true);
-					};
+	</script>
 
-			});
-	
-	});
-		
-});
-</script>
-	
-
-<style>
-
-	.post { position: absolute;text-align: center; display: none; overflow: hidden }
-	img { margin: 0; padding: 0 }
-
-</style>
-
-	<?php /* The HTML5 Shim is required for older browsers, mainly older versions IE */ ?>
-	<!--[if lt IE 9]>
-		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
 	<?php wp_head(); ?> <?php /* this is used by many Wordpress features and for plugins to work proporly */ ?>
 
   <!-- CSS -->
