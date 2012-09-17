93741b836a2732a6e56e218e0d2d25dce3dde363
jie lian <imjeee@gmail.com>
2012-09-17 04:34:46 -0500

    good place

--- wp-content/themes/imjeee2012/sidebar.php
+++ wp-content/themes/imjeee2012/sidebar.php
@@ -1,7 +1,7 @@
 <nav>
 	<div id="logo">
 		<a href="<?php echo get_settings('home'); ?>"><h1>imjeee</h1>
-		<h3>design + development</a></h3>
+		<h3>design + development</h3></a>
   </div>
 		<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ));  ?>
 </nav>
