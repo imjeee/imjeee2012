a90ebca08b77c812df3762b9ad5038211c4a1b0e
jie lian <imjeee@gmail.com>
2012-09-27 10:41:40 -0500

    more scss

--- wp-content/themes/imjeee2012/sidebar.php
+++ wp-content/themes/imjeee2012/sidebar.php
@@ -1,26 +1,30 @@
+<div class="logo">
+  <a href="<?php echo get_settings('home'); ?>">
+	  <div class="logo-top">imjeee</div>
+	  <div class="logo-bottom">design + development</div>
+  </a>
+</div>
+
 <nav>
-	<div id="logo">
-		<a href="<?php echo get_settings('home'); ?>"><h1>imjeee</h1>
-		  <h3>design + development</h3></a>
-  </div>
 	<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ));  ?>
   <ul><li><a class="contact-btn">contact</a></li></ul>
-
-  <div id="contact-box">
-    <h1>Get in touch with Jie</h1>
-    <ul class="social" id="css3">
-      <li class="linkedin"><a href="http://www.linkedin.com/pub/jie-lian/47/762/b74" target="_blank"><strong>linkedIn</strong></a></li>
-      <li class="twitter"><a href="http://www.twitter.com/imjeee" target="_blank"><strong>twitter</strong></a></li>
-      <li class="tumblr"><a href="http://www.imjeee.tumblr.com/" target="_blank"><strong>tumblr</strong></a></li>
-      <li class="deviantart"><a href="http://www.imjeee.deviantart.com" target="_blank"><strong>deviantart</strong></a></li>
-    </ul>
-    <form id="contact-form"><!-- method="post" action="http://imjeee.com/wp-content/themes/imjeee2012/process.php"-->
-      <input type="text" name="name" id="senderName" placeholder="Your name" required="required" maxlength="40" />
-      <input type="email" name="email" id="senderEmail" placeholder="Your email" required="required" maxlength="50" />
-      <textarea name="message" id="message" placeholder="Please type your message..." required="required" rows="10" maxlength="10000"></textarea>
-      <button name="sendButton" id="submit" type="submit">Send Email</button>
-      <div class="ajax"><p>SENDING</p></div>
-    </form>      
-  </div>
-  <div id="cover">&nbsp;</div>
 </nav>
+
+<div id="contact-box">
+  <h1>Get in touch with Jie</h1>
+  <ul class="social" id="css3">
+    <a href="http://www.linkedin.com/pub/jie-lian/47/762/b74" target="_blank"><li class="linkedin"><strong>linkedIn</strong></li></a>
+    <a href="http://www.twitter.com/imjeee" target="_blank"><li class="twitter"><strong>twitter</strong></li></a>
+    <a href="http://www.imjeee.tumblr.com/" target="_blank"><li class="tumblr"><strong>tumblr</strong></li></a>
+    <a href="http://www.imjeee.deviantart.com" target="_blank"><li class="deviantart"><strong>deviantart</strong></li></a>
+  </ul>
+  <form id="contact-form"><!-- method="post" action="http://imjeee.com/wp-content/themes/imjeee2012/process.php"-->
+    <input type="text" name="name" id="senderName" placeholder="Your name" required="required" maxlength="40" />
+    <input type="email" name="email" id="senderEmail" placeholder="Your email" required="required" maxlength="50" />
+    <textarea name="message" id="message" placeholder="Please type your message..." required="required" rows="10" maxlength="10000"></textarea>
+    <button name="sendButton" id="submit" type="submit">Send Email</button>
+    <div class="ajax"><p>SENDING</p></div>
+  </form>      
+</div>
+
+<div id="cover">&nbsp;</div>
--- wp-content/themes/imjeee2012/work.php
+++ wp-content/themes/imjeee2012/work.php
@@ -5,9 +5,9 @@
    ?>
 <style type="text/css">
   body {
-    background-image: url('<?php bloginfo('template_url'); ?>/images/background-work.png');
-    background-repeat: no-repeat;
-    background-attachment: fixed;
+  background-image: url('<?php bloginfo('template_url'); ?>/images/background-work.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
   }
 </style>
 
@@ -20,26 +20,23 @@
     <div id="cat_nav">
       <ul class="splitter">
         <li class="segment-0 selected-0 selected"><a href="#" data-value="all">All</a></li>
-        <?php
-           $fineart = get_category_by_slug('fine-art')->term_id;
+        <?php $fineart = get_category_by_slug('fine-art')->term_id;
         $args = array( 'exclude' => $fineart);
         $categories = get_categories( $args );
-              for($i = 0; $i < count($categories); ++$i){ ?>
-              <li class="segment-<?php echo ($i + 1) ?>">
-                <a href="#" data-value="<?php echo $categories[$i]->slug; ?>">
-                  <?php echo $categories[$i]->cat_name; ?>
-                </a>
-              </li>
-              <?php } ?>
-      </ul>
-    </div>
+        for($i = 0; $i < count($categories); ++$i){ ?>
+        <li class="segment-<?php echo ($i + 1) ?>">
+          <a href="#" data-value="<?php echo $categories[$i]->slug; ?>">
+            <?php echo $categories[$i]->cat_name; ?>
+          </a>
+        </li>
+        <?php } ?>
+      </ul><!-- .splitter -->
+    </div><!-- #cat_nav -->
 		<div id="serious-work">
       <ul id="list" class="img-grid">
-				<?php 
-        $posts_array = query_posts('posts_per_page=-1&cat=-' . $fineart);
-        for ($i = 0; $i < count($posts_array); ++$i){
-                          $post = $posts_array[$i];
-                          $post_cats = get_the_category(); ?>
+				<?php $posts_array = query_posts('posts_per_page=-1&cat=-' . $fineart);
+              foreach( $posts_array as $post){
+              $post_cats = get_the_category(); ?>
         <li data-id="id-<?php echo $i ?>" class="<?php foreach($post_cats as $cat) { echo $cat->slug . " "; } ?>">
 				  <?php if ( has_post_thumbnail() ): ?>
 		      <div class="work-grid">
@@ -57,5 +54,5 @@
   </div> <!-- #right -->
 </body>
 <script src="<?php bloginfo( 'template_url' ); ?>/js/jquery.quicksand.js"></script><!-- serious work quicksand -->
-<script src="<?php bloginfo( 'template_url' ); ?>/js/serious-work.js"></script><!-- serious work quicksand -->
+<script src="<?php bloginfo( 'template_url' ); ?>/js/work.js"></script><!-- work quicksand -->
 <?php get_footer(); /* this is used by many Wordpress features and plugins to work proporly */ ?>
